e6fbae69758a8bd975d889e50377b182861706c4
0xA672 <258348964+0xA672@users.noreply.github.com>
2026-04-11 10:35:20 +0800

    Add magenta and cyan colors to Colors enum

--- mirror.hpp
+++ mirror.hpp
@@ -78,6 +78,8 @@ enum class Colors {
   black,
   green,
   blue,
+  magenta,
+  cyan,
   red,
   white,
   yellow,
@@ -103,8 +105,11 @@ inline const char* MatchLv(Lv lv){
 
 inline const char* colorCode(Colors col) {
     switch(col) {
+        case Colors::black:  return "\033[30m";
         case Colors::green:  return "\033[32m";
         case Colors::blue:   return "\033[34m";
+        case Colors::magenta:return "\033[35m";
+        case Colors::cyan:   return "\033[36m";        
         case Colors::red:    return "\033[31m";
         case Colors::white:  return "\033[37m";
         case Colors::yellow: return "\033[33m";
@@ -142,6 +147,100 @@ inline void mirprint(Lv lv, const std::string& msg, Mode mode, Colors col = Colo
             break;
         }
     }
+}
+
+enum class Style : uint8_t {
+    None      = 0,
+    Bold      = 1 << 0,
+    Dim       = 1 << 1,
+    Italic    = 1 << 2,
+    Underline = 1 << 3,
+    Blink     = 1 << 4,
+    Reverse   = 1 << 5,
+    Hidden    = 1 << 6,
+    Strikethrough = 1 << 7
+};
+
+inline Style operator|(Style a, Style b) {
+    return static_cast<Style>(static_cast<uint8_t>(a) | static_cast<uint8_t>(b));
+}
+
+inline bool operator&(Style a, Style b) {
+    return (static_cast<uint8_t>(a) & static_cast<uint8_t>(b)) != 0;
+}
+
+class ColoredString {
+public:
+    explicit ColoredString(std::string s) : str_(std::move(s)) {}
+
+    ColoredString& black()   { fg_ = Colors::black; return *this; }
+    ColoredString& red()     { fg_ = Colors::red;   return *this; }
+    ColoredString& green()   { fg_ = Colors::green; return *this; }
+    ColoredString& yellow()  { fg_ = Colors::yellow;return *this; }
+    ColoredString& blue()    { fg_ = Colors::blue;  return *this; }
+    ColoredString& magenta() { fg_ = Colors::magenta; return *this; }
+    ColoredString& cyan()    { fg_ = Colors::cyan;  return *this; }
+    ColoredString& white()   { fg_ = Colors::white; return *this; }
+
+    ColoredString& on_black()   { bg_ = Colors::black; return *this; }
+    ColoredString& on_red()     { bg_ = Colors::red;   return *this; }
+    ColoredString& on_green()   { bg_ = Colors::green; return *this; }
+    ColoredString& on_yellow()  { bg_ = Colors::yellow;return *this; }
+    ColoredString& on_blue()    { bg_ = Colors::blue;  return *this; }
+    ColoredString& on_magenta() { bg_ = Colors::magenta; return *this; }
+    ColoredString& on_cyan()    { bg_ = Colors::cyan;  return *this; }
+    ColoredString& on_white()   { bg_ = Colors::white; return *this; }
+
+    ColoredString& bold()          { style_ = style_ | Style::Bold; return *this; }
+    ColoredString& dim()           { style_ = style_ | Style::Dim; return *this; }
+    ColoredString& italic()        { style_ = style_ | Style::Italic; return *this; }
+    ColoredString& underline()     { style_ = style_ | Style::Underline; return *this; }
+    ColoredString& blink()         { style_ = style_ | Style::Blink; return *this; }
+    ColoredString& reverse()       { style_ = style_ | Style::Reverse; return *this; }
+    ColoredString& hidden()        { style_ = style_ | Style::Hidden; return *this; }
+    ColoredString& strikethrough() { style_ = style_ | Style::Strikethrough; return *this; }
+
+    ColoredString& clear_style() { style_ = Style::None; return *this; }
+
+    friend std::ostream& operator<<(std::ostream& os, const ColoredString& cs) {
+        vtMODE();  
+        
+        std::string seq = "\033[";
+        bool first = true;
+        
+        if (cs.fg_ != Colors::Auto) {
+            seq += (first ? "" : ";") + std::to_string(30 + static_cast<int>(cs.fg_));
+            first = false;
+        }
+
+        if (cs.bg_ != Colors::Auto) {
+            seq += (first ? "" : ";") + std::to_string(40 + static_cast<int>(cs.bg_));
+            first = false;
+        }
+
+        if (cs.style_ & Style::Bold)      { seq += (first ? "" : ";") + "1"; first = false; }
+        if (cs.style_ & Style::Dim)       { seq += (first ? "" : ";") + "2"; first = false; }
+        if (cs.style_ & Style::Italic)    { seq += (first ? "" : ";") + "3"; first = false; }
+        if (cs.style_ & Style::Underline) { seq += (first ? "" : ";") + "4"; first = false; }
+        if (cs.style_ & Style::Blink)     { seq += (first ? "" : ";") + "5"; first = false; }
+        if (cs.style_ & Style::Reverse)   { seq += (first ? "" : ";") + "7"; first = false; }
+        if (cs.style_ & Style::Hidden)    { seq += (first ? "" : ";") + "8"; first = false; }
+        if (cs.style_ & Style::Strikethrough) { seq += (first ? "" : ";") + "9"; first = false; }
+        
+        seq += "m";
+        os << seq << cs.str_ << "\033[0m";
+        return os;
+    }
+
+private:
+    std::string str_;
+    Colors fg_ = Colors::Auto;
+    Colors bg_ = Colors::Auto;
+    Style style_ = Style::None;
+};
+
+inline ColoredString colored(std::string s) {
+    return ColoredString(std::move(s));
 }
 
 } // namespace mirror
